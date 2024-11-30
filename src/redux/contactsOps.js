61e14cdb2138e1d50d4b49eeeefbaec7f31d8c4f
vanok00 <vanok008@ukr.net>
2024-11-30 17:09:13 +0200

    firststep

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -1,52 +1,39 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.baseURL = 'https://6706b946a0e04071d22809d7.mockapi.io'
-export const fetchContacts = createAsyncThunk('fetchData', async(_,thunkApi) =>{
+axios.defaults.baseURL = "https://connections-api.goit.global";
+export const fetchContacts = createAsyncThunk(
+  "fetchData",
+  async (_, thunkApi) => {
     try {
-        const {data} = await axios.get('/contacts')  
-        return data
+      const { data } = await axios.get("/contacts");
+      return data;
     } catch (error) {
-        return thunkApi.rejectWithValue(error.message)
+      return thunkApi.rejectWithValue(error.message);
     }
-})
+  }
+);
 
-export const deleteContactThunk = createAsyncThunk('deleteContact', async(id,thunkApi) =>{
+export const deleteContactThunk = createAsyncThunk(
+  "deleteContact",
+  async (contactId, thunkApi) => {
     try {
-        const {data} = await axios.delete(`/contacts/${id}`)  
-        return data.id
+      const { data } = await axios.delete(`/contacts/${contactId}`);
+      return data.id;
     } catch (error) {
-        return thunkApi.rejectWithValue(error.message)
+      return thunkApi.rejectWithValue(error.message);
     }
-})
+  }
+);
 
-export const addContactThunk = createAsyncThunk('addContact', async(body,thunkApi) =>{
+export const addContactThunk = createAsyncThunk(
+  "addContact",
+  async (body, thunkApi) => {
     try {
-        const {data} = await axios.post(`/contacts`, body);  
-        return data
+      const { data } = await axios.post(`/contacts`, body);
+      return data;
     } catch (error) {
-        return thunkApi.rejectWithValue(error.message)
+      return thunkApi.rejectWithValue(error.message);
     }
-})
-
-// export const fetchContacts = () => async dispatch => {
-//    try {
-//     dispatch(setIsLoading(true))
-//     const {data} = await axios.get('/contacts')
-//     dispatch(fetchContactsSuccess(data));
-//    } catch  {
-//     dispatch(setError(true))
-//    }
-// }
-
-// export const deleteContactThunk = id => async dispatch => {
-//     const {data} = await axios.delete(`/contacts/${id}`)
-//     dispatch(deleteContact(data.id));
-//     }
-
-    // export const addContactThunk = body => async dispatch => {
-    //     const {data} = await axios.post(`/contacts`, body)
-    //     dispatch(addContact(data));
-    //     }
-
-   
+  }
+);
